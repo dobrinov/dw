6a5aca8921ddfe543fc34e8d6e0b2e2275e05540
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2018-05-21 09:27:49 +0300

    Approve jobs automatically

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,7 +17,7 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new job_params.merge(sent_for_review_at: Time.current)
+    @job = Job.new job_params.merge(sent_for_review_at: Time.current, approved_at: Time.current)
 
     if @job.save
       redirect_to jobs_path, notice: "Your job post has been sent to approval. You will be notified at #{@job.company_contact_email} when it is approved."
